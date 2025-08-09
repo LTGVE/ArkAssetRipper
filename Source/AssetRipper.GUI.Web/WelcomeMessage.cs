50563d050e3e6155ac821f621085c239448e6e0e
LT_GVE <1638666724@qq.com>
2025-08-09 21:10:33 +0800

    Change console title and texts

--- Source/AssetRipper.GUI.Web/WelcomeMessage.cs
+++ Source/AssetRipper.GUI.Web/WelcomeMessage.cs
@@ -3,19 +3,17 @@
 public static class WelcomeMessage
 {
 	private const string AsciiArt = """
-		                       _   _____  _                       
-		    /\                | | |  __ \(_)                      
-		   /  \   ___ ___  ___| |_| |__) |_ _ __  _ __   ___ _ __ 
-		  / /\ \ / __/ __|/ _ \ __|  _  /| | '_ \| '_ \ / _ \ '__|
-		 / ____ \\__ \__ \  __/ |_| | \ \| | |_) | |_) |  __/ |   
-		/_/    \_\___/___/\___|\__|_|  \_\_| .__/| .__/ \___|_|   
-		                                   | |   | |              
-		                                   |_|   |_|              
+		    _         _        _                 _   ____  _                       
+		   / \   _ __| | __   / \   ___ ___  ___| |_|  _ \(_)_ __  _ __   ___ _ __ 
+		  / _ \ | '__| |/ /  / _ \ / __/ __|/ _ \ __| |_) | | '_ \| '_ \ / _ \ '__|
+		 / ___ \| |  |   <  / ___ \\__ \__ \  __/ |_|  _ <| | |_) | |_) |  __/ |   
+		/_/   \_\_|  |_|\_\/_/   \_\___/___/\___|\__|_| \_\_| .__/| .__/ \___|_|   
+		                                                    |_|   |_|              
 		""";
 
 	private const string Directions = """
-		In a moment, a line will appear: "Now listening on:" followed by a url.
-		Open that url in any web browser to access the AssetRipper user interface.
+		一会后,会出现"Now listening on:",点击URL
+		打开后就会看见ArkAssetRipper的欢迎界面,点击"Language"切换语言,点击"文件"-"加载文件"即可加载.
 		""";
 
 	public static void Print()
